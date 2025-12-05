1bbb978a8c6c9a5410b372ac763d6d68cdc80c22
LeonardoAleee <alexandreleonardo269@gmail.com>
2025-12-05 15:54:01 -0300

    Add Composite base structure

--- tree_design.py
+++ tree_design.py
@@ -1,3 +1,21 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List, Optional
+
+#######################################################################
+# Composite: Node, DecisionNode, LeafNode
+#######################################################################
+
+class Node(ABC):
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self._parent: Optional[Node] = None
+
+    @abstractmethod
+    def is_leaf(self) -> bool: ...
+
+    def set_parent(self, parent: Optional[Node]) -> None:
+        self._parent = parent
+
+    @abstractmethod
+    def accept(self, visitor: "Visitor") -> None: ...
